ff803a19325da6bc3f6f15d7004c69d2d8f1bf12
Pinderr1 <pinderdhami1@gmail.com>
2025-09-17 10:01:13 -0700

    Update Babel config plugins

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'expo-router/babel',
       'react-native-worklets/plugin',
-      'react-native-reanimated/plugin',
     ],
   };
 };
